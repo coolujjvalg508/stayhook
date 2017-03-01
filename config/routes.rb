1c9800560c88429ed8f569ee4c69c412c5061d92
ujjval <ujjval@brsoftech.org>
2017-03-01 18:14:32 +0530

    firstupload

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  / begin
-    ActiveAdmin.routes(self)
-  rescue Exception => e
-    puts "ActiveAdmin: #{e.class}: #{e}"
-  end/
+   #begin
+    #ActiveAdmin.routes(self)
+  #rescue Exception => e
+   # puts "ActiveAdmin: #{e.class}: #{e}"
+  #end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
