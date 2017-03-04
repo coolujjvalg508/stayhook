791ad74f6bf96de4ec044e9613b73c22ef53ba8f
ujjval <ujjval@brsoftech.org>
2017-03-04 11:32:17 +0530

    new

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ApplicationMailer
 
 	  # send a signup email to the user, pass in the user object that   contains the user's email address
 	  def send_signup_email(vendor)
-	  	abort(ENV['DEFAULT_FROM_EMAIL'].to_json)
+	  	#abort(ENV['DEFAULT_FROM_EMAIL'].to_json)
 	    @vendor = vendor
 	    #abort(@vendor.email.to_json)
 	    mail( :to => @vendor.email,
