f47e81ecd27bb3b40629cd9c14abd5ce2930abb5
ujjval <ujjval@brsoftech.org>
2017-03-04 11:10:36 +0530

    new

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,10 @@
 class UserMailer < ApplicationMailer
 	  default :from => "StayHook  <#{ ENV['DEFAULT_FROM_EMAIL'] }>"
 
+
 	  # send a signup email to the user, pass in the user object that   contains the user's email address
 	  def send_signup_email(vendor)
+	  	abort(ENV['DEFAULT_FROM_EMAIL'].to_json)
 	    @vendor = vendor
 	    #abort(@vendor.email.to_json)
 	    mail( :to => @vendor.email,
